a1286287243a7daa2278428bff9517f7e7a0fad8
espadayass <amanyamar505@gmail.com>
2024-05-19 02:12:00 +0100

    correction dans le fichier web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,4 @@
-<?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
-
-// require __DIR__.'/auth.php';
-
-
-use App\Http\Controllers\FormateurController;
-use App\Http\Controllers\GroupePhysiqueController;
-Route::post('/formateurs', [FormateurController::class, 'store']);
-Route::resource('groupe',GroupePhysiqueController::class);
 <?php
 
 use Illuminate\Support\Facades\Route;
