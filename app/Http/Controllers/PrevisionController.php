326974bc8ac83e48020c0764a147b127b48cbb7e
aliiexe <alibusinessbourak@gmail.com>
2024-07-01 17:26:47 +0100

    finale code of prevision

--- app/Http/Controllers/PrevisionController.php
+++ app/Http/Controllers/PrevisionController.php
@@ -33,83 +33,8 @@ class PrevisionController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    
-    // public function store(Request $request)
-    // {
-    //     $groupe = groupe_physique::where('id', $request->idGroupePhysique)->get();
-    //     $affectation = affectation_formodgr::where('idGroupePhysique', $request->idGroupePhysique)->get();
-    //     $annee = AnneeFormation::where('anneeFormation', $request->anneeFormation)->first();
-    //     $maxOrdreModule = Module::max('ordreModule');
-    //     foreach ($affectation as $affect) {
-    //         $id = $affect->id;
-    //         $heuresSemaine = $affect->heureSemaine;
-    //         $dateDebutAnnee = new DateTime($annee->dateDebutAnneeFormation);
-    //         $dateFinAnnee = new DateTime($annee->dateFinAnneeFormation);
-    //         $dateDebut2Semestre = new DateTime($annee->dateDebut2Semestre);
-    //         $dateDebutFirstModule = $dateDebutAnnee;
-    //         $dateDebutModule = null;
-    //         $var = 2;
-    //         while ($var < $maxOrdreModule) {
-    //             $modulesArray = $affect->module()->where('ordreModule', '=', $var)->orderBy('MHT', 'ASC')->get();
-    //             foreach ($modulesArray as $module) {
-    //                 $MHTmodule = $module->MHT;
-    //                 $ordreModule = $module->ordreModule;
-    //                 $NbrSeanceSemaine = $MHTmodule / $heuresSemaine;
-    //                 $dateDebutModule = $dateDebutFirstModule;
-    //                 $dateDebutModule->modify('+' . ($NbrSeanceSemaine * 7) . ' days');
-    //                 $dateDebutModule = $dateDebutModule->format('Y-m-d');
 
-    //                 $dateFinModule = new DateTime($dateDebutModule);
-    //                 $dateFinModule->modify('+' . ($NbrSeanceSemaine * 7) . ' days');
-    //                 $dateFinModule = $dateFinModule->format('Y-m-d');
-
-    //                 $dateCC1 = new DateTime($dateDebutModule);
-    //                 prevision::create([
-    //                     "affectationid" => $id,
-    //                     "datedebutmodule" => $dateDebutModule,
-    //                     "datefinmodule" => $dateFinModule
-    //                 ]);
-    //                 $heure = 0;
-    //                 while ($heure <= 25) {
-    //                     prevision::where('affectationid', $id)->update([
-    //                         "datecc1" => $dateCC1."+ interval 1 week"
-    //                     ]);
-    //                     $heure += $affect->heureAffectationParSemaine;
-    //                 }
-    //                 $dateCC1 = $dateCC1->format('Y-m-d');
-    //                 $dateCC2 = new DateTime($dateCC1);
-    //                 $heure = 0;
-    //                 while ($heure <= 25) {
-    //                      prevision::where('affectationid', $id)->update([
-    //                         "datecc2" => $dateCC1."+ interval 1 week"
-    //                     ]);
-    //                     $heure += $affect->heureAffectationParSemaine;
-    //                 }
-    //                 $dateCC2 = $dateCC2->format('Y-m-d');
-    //                 $dateCC3 = new DateTime($dateCC2);
-    //                 if ($dateCC3 <= new DateTime($dateFinModule)) {
-    //                     $heure = 0;
-    //                     while ($heure <= 25) {
-    //                          prevision::where('affectationid', $id)->update([
-    //                         "datecc3" => $dateCC2."+ interval 1 week"
-    //                     ]);
-    //                         $heure += $affect->heureAffectationParSemaine;
-    //                     }
-    //                 }
-    //                 $dateCC3 = $dateCC3->format('Y-m-d');
-                    
-    //                 $dateEFM = new DateTime($dateCC3);
-    //                 $dateEFM->modify('+' . $NbrSeanceSemaine . ' weeks');
-    //                 $dateEFM = $dateEFM->format('Y-m-d');
-                    
-    //                 $dateDebutFirstModule = $dateFinModule;
-    //             }
-    //             $var++;
-    //         }
-    //     }
-    // }
-
-    public function store(Request $request)
+public function store(Request $request)
 {
     $groupe = groupe_physique::where('id', $request->idGroupePhysique)->get();
     $affectation = affectation_formodgr::where('idGroupePhysique', $request->idGroupePhysique)->get();
@@ -123,30 +48,31 @@ class PrevisionController extends Controller
         $dateDebutFirstModule = clone $dateDebutAnnee;
         $var = 2;
 
-        while ($var < $maxOrdreModule) {
+        // Initialize preceding module's dateFinModule
+        $precedingDateFinModule = null;
+
+        while ($var <= $maxOrdreModule) {
             $modulesArray = $affect->module()->where('ordreModule', $var)->orderBy('MHT', 'ASC')->get();
 
-            foreach ($modulesArray as $module) {
+            foreach ($modulesArray as $index => $module) {
                 $MHTmodule = $module->MHT;
                 $NbrSeanceSemaine = (int)($MHTmodule / $heuresSemaine);
 
                 // Calculate the dateDebutModule
-                if ($module->ordreModule <= $var) {
+                if ($index == 0 && $module->ordreModule == $var) {
                     $dateDebutModule = clone $dateDebutAnnee;
                 } else {
-                    $dateDebutModule = clone $dateDebutFirstModule;
-                    $daysToAdd = $NbrSeanceSemaine * 7;
-                    $dateDebutModule->modify('+' . $daysToAdd . ' days');
+                    $dateDebutModule = clone $precedingDateFinModule;
                 }
 
                 // Calculate the dateFinModule
-                $dateFinModule = clone $dateDebutModule;
-                $dateFinModule->modify('+' . $NbrSeanceSemaine * 7 . ' days');
+                $dateFinModule = clone $dateDebutFirstModule;
+                $dateFinModule->modify('+' . ($NbrSeanceSemaine * 7) . ' days');
 
                 // Create prevision entry
                 prevision::create([
                     "affectationid" => $id,
-                    "datedebutmodule" => $dateDebutModule->format('Y-m-d'),
+                    "datedebutmodule" => $dateDebutFirstModule->format('Y-m-d'),
                     "datefinmodule" => $dateFinModule->format('Y-m-d')
                 ]);
 
@@ -183,14 +109,17 @@ class PrevisionController extends Controller
                     ]);
                 }
 
-                // Calculate and update dateEFM
-                $dateEFM = clone $dateCC3;
-                $weeksToAdd = (int)$NbrSeanceSemaine;
-                $dateEFM->modify('+' . $weeksToAdd . ' weeks');
+                // Calculate and update dateEFM (Examen Fin Module)
+                $dateEFM = clone $dateFinModule;
+                $dateEFM->modify('+1 week');
                 $dateEFMFormatted = $dateEFM->format('Y-m-d');
 
-                // Update dateDebutFirstModule for the next iteration
-                $dateDebutFirstModule = clone $dateFinModule;
+                prevision::where('affectationid', $id)->update([
+                    "dateEFM" => $dateEFMFormatted
+                ]);
+
+                // Update precedingDateFinModule for the next iteration
+                $precedingDateFinModule = clone $dateFinModule;
 
                 // Debugging statements to check date values
                 Log::debug("ID: $id, dateDebutModule: " . $dateDebutModule->format('Y-m-d'));
@@ -199,13 +128,13 @@ class PrevisionController extends Controller
                 Log::debug("ID: $id, dateCC2: " . $dateCC2->format('Y-m-d'));
                 Log::debug("ID: $id, dateCC3: " . $dateCC3->format('Y-m-d'));
                 Log::debug("ID: $id, dateEFM: " . $dateEFMFormatted);
-                
             }
+            $dateDebutFirstModule = $dateFinModule;
             $var++;
-
         }
     }
 }
+
 
     /**
      * Display the specified resource.
